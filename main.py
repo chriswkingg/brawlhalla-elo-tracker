6957f74243d2d1484f8dd587a96f28648b0d890b
chriswkingg <christopherking@rogers.com>
2021-02-09 12:18:28 -0500

    Added writing to file

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import requests as r
 username = "twitch.tv/lawlbottv"
 reigon = "us-e"
 apiKey = ""
+f = open("brawlelo.txt", "w")
 
-request = r.get(url = "https://api.brawlhalla.com/rankings/1v1/" + reigon + "/1", params = {'name':username, "api_key":apiKey})
-print(request.json().split(','))
+data = r.get(url = "https://api.brawlhalla.com/rankings/1v1/" + reigon + "/1", params = {'name':username, "api_key":apiKey}).json()[0]
+currentElo = data["rating"]
+startingElo = currentElo
+
+f.write("Current Elo: " + str(currentElo) + " Elo Change: " + str(startingElo - currentElo))
+print(data["rating"])
