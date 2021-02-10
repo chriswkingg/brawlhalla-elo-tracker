fd7511a64afb885008372942e4856c3215c15655
chriswkingg <christopherking@rogers.com>
2021-02-10 09:02:17 -0500

    Changed request and added support for 2s

--- main.py
+++ main.py
@@ -1,31 +1,72 @@
 import requests as r
-import sched, time
+import sched, time, sys
 
 #Global vars
 s = sched.scheduler(time.time, time.sleep)
-username = "twitch.tv/lawlbottv"
-reigon = "us-e"
+firstID = ""
+secondID = ""
 apiKey = ""
 currentElo = 0
 startingElo = 0
 
-#Makes api request and writes elo and change to file
-def getElo():
-    currentElo = r.get(url = "https://api.brawlhalla.com/rankings/1v1/" + reigon + "/1", params = {'name':username, "api_key":apiKey}).json()[0]['rating']
+def writeElo():
+    global startingElo, currentElo
+    print("Elo: " + str(currentElo))
     f = open("brawlelo.txt", "w")
     f.write("Current Elo: " + str(currentElo) + " Elo Change: " + str(currentElo - startingElo))
     f.close()
-    print("Current Elo:", currentElo)
-    s.enter(60, 1, getElo)
+
+def getOnes():
+    global startingElo, currentElo, firstID, apiKey
+    currentElo = r.get(url = "https://api.brawlhalla.com/player/" + firstID + "/ranked", params = {"api_key":apiKey}).json()["rating"]
+    writeElo()
+    s.enter(60, 1, getOnes)
+
+def getTwos():
+    global startingElo, currentElo, firstID, secondID, apiKey
+    response = r.get(url = "https://api.brawlhalla.com/player/" + firstID + "/ranked", params = {"api_key":apiKey}).json()
+    for i in response["2v2"]:
+        if(i["brawlhalla_id_two"] == secondID):
+            currentElo = i["rating"]
+            break
+    writeElo()
+    
+    s.enter(60, 1, getTwos)    
+
+def startOnes():
+    global startingElo, currentElo, firstID, apiKey
+    response = r.get(url = "https://api.brawlhalla.com/player/" + firstID + "/ranked", params = {"api_key":apiKey}).json()
+    startingElo = response["rating"]
+    currentElo = startingElo
+    writeElo()
+    
+    s.enter(60, 1, getOnes)
+    s.run()
+
+def startTwos():
+    global startingElo, currentElo, firstID, secondID, apiKey
+    response = r.get(url = "https://api.brawlhalla.com/player/" + firstID + "/ranked", params = {"api_key":apiKey}).json()
+    for i in response["2v2"]:
+        print("Teamname: " + i["teamname"] + " SecondID: " + str(i["brawlhalla_id_two"]))
+
+    secondID = int(input("Enter second ID: "))
+    for i in response["2v2"]:
+        if(i["brawlhalla_id_two"] == secondID):
+            startingElo = i["rating"]
+            currentElo = startingElo
+            break
+    writeElo()
+    
+    s.enter(60, 1, getTwos)
+    s.run()
 
 #Main
-startingElo = r.get(url = "https://api.brawlhalla.com/rankings/1v1/" + reigon + "/1", params = {'name':username, "api_key":apiKey}).json()[0]['rating']
-currentElo = startingElo
-print("Starting Elo:", startingElo)
-f = open("brawlelo.txt", "w")
-f.write("Current Elo: " + str(currentElo) + " Elo Change: " + str(startingElo - currentElo))
-f.close()
-
-#Scheduler to run getElo every minute
-s.enter(60, 1, getElo)
-s.run()
+if(len(sys.argv) != 4):
+    print("Incorrect number arguments:\n python main.py bracket brawlID apiKey")
+    exit()
+firstID = sys.argv[2]
+apiKey = sys.argv[3]
+if(sys.argv[1] == "1s"):
+    startOnes()
+elif(sys.argv[1] == "2s"):
+    startTwos()
